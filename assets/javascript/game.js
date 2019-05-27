c72e4b64dfbf5ca43e5880a3825ac1e607f9b1d7
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-05-27 14:23:15 -0700

    figured out how to get number of wins and losses to change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,39 +3,87 @@ $(document).ready(function(){
     var random = Math.floor(Math.random() * 101) + 19;
     $("#random-number").text(random);
 //want to generate random numbers in the 1-12 range for each of the crystals
-   var rubyNum = Math.floor(Math.random() * 11) + 1;
-   var blueNum =  Math.floor(Math.random() * 11) + 1;
-   var emeraldNum =  Math.floor(Math.random() * 11) + 1;
-   var purpleNum =  Math.floor(Math.random() * 11) + 1;
+   var rubyNum = Math.floor(Math.random() * 12) + 1;
+   console.log(rubyNum);
+   var blueNum =  Math.floor(Math.random() * 12) + 1;
+   console.log(blueNum);
+   var emeraldNum =  Math.floor(Math.random() * 12) + 1;
+   console.log(emeraldNum);
+   var purpleNum =  Math.floor(Math.random() * 12) + 1;
+   console.log(purpleNum);
 
 //making initial variables for total score, wins, and losses
    var totalScore = 0;
-   $("#totalScore").text(0);
+   var wins = 0;
+   var losses = 0;
 
-   var winCount = 0;
-   var lossCount = 0;
+   
+   $("#winCount").text(wins);
+   $("#lossCount").text(losses);
 
- 
 
+
+   
 //adding click triggers for gems 
 //ruby gem
 $("#ruby-gem").on("click", function(){
     totalScore = totalScore + rubyNum;
     $("#totalScore").text(totalScore);
+    console.log(totalScore);
+    if(totalScore == random){
+        wins++;
+        $("#winCount").text(wins);
+        console.log(wins);
+        } 
+    else if(totalScore > random){
+        losses++;
+        $("#lossCount").text(losses);
+        console.log(losses);
+        }
 })
 $("#blue-gem").on("click", function(){
     totalScore = totalScore + blueNum;
     $("#totalScore").text(totalScore);
+    if(totalScore == random){
+        wins++;
+        $("#winCount").text(wins);
+        console.log(wins);
+        }
+    else if(totalScore > random){
+        losses++;
+        $("#lossCount").text(losses);
+        console.log(losses);
+        }
 })
 
 $("#purple-gem").on("click", function(){
     totalScore = totalScore + purpleNum;
     $("#totalScore").text(totalScore);
+    if(totalScore == random){
+        wins++;
+        $("#winCount").text(wins);
+        console.log(wins);
+        }
+    else if(totalScore > random){
+        losses++;
+        $("#lossCount").text(losses);
+        console.log(losses);
+        }
 })
 
 $("#emerald-gem").on("click", function(){
     totalScore = totalScore + emeraldNum;
     $("#totalScore").text(totalScore);
+    if(totalScore == random){
+        wins++;
+        $("#winCount").text(wins);
+        console.log(wins);
+        }
+    else if(totalScore > random){
+        losses++;
+        $("#lossCount").text(losses);
+        console.log(losses);
+        }
 })
 
 })
